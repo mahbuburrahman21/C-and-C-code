44530ba303105bd08cd446d8bec43c735e66fe5f
Md Mahbubur Rahman <rahman22205341070@diu.edu.bd>
2024-06-19 14:47:00 +0600

    Create Sum_Of_Two_Value_Equal_From_An_Array.c
    This program will check all the value which two value are equal to a given value x.  If it found then it will print the two value and it's sum. Otherwise print not possible.

---
+++ Sum_Of_Two_Value_Equal_From_An_Array.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main()
+{
+    int n, num, i, j;
+    
+    scanf("%d %d", &n, &num);
+    int ar[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &ar[i]);
+    }
+
+
+    int flag = 0;    
+    for (int i = 0; i < n-1; i++)
+    {
+        for (int j = i+1; j < n; j++)
+        {
+            if (ar[i]+ar[j] == num)
+            {
+                flag = 1;
+                printf("%d + %d = %d", ar[i], ar[j], ar[i]+ar[j]);
+            }
+            
+        }
+        
+    }
+    if (flag == 0)
+    {
+        printf("Not possible");
+    }
+    return 0; 
+}
